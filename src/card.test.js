3026e4ab9f596e8e772f4072cf02520e770e08fe
Aaron Zemetres <azemetre@gmail.com>
2020-07-06 12:35:41 -0400

    chore: todo for fixing tests with oclif

--- src/card.test.js
+++ src/card.test.js
@@ -13,10 +13,10 @@ const box = `
        +-------------------------------------------------------+
 `;
 
+// TODO: fix state of current test
+// a very poor example of a CLI test, may need to integrate with @oclif/test
 test("business card is looking slick", () => {
   const consoleSpy = jest.spyOn(console, "log");
-  // console.log = jest.fn();
   console.log(card);
   expect(consoleSpy).toHaveBeenCalledTimes(1);
-  // expect(consoleSpy).toHaveBeenCalledWith(box);
 });
